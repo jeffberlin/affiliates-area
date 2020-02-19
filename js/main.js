26aea7613af71a651e028abac68dc3e8ef48b557
jeffberlin <jeff@jbrally.com>
2020-02-19 12:35:16 -0500

    Building main.html, adding css files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-function hello()[
-    console.log();
-
-]
+// Redirect to login page if this page is accessed and the vendor is not logged in
+if (getCookieValue ("BMTMicro.Affiliates.AffiliateID") == null) {
+  window.location = "https://affiliates-new.bmtmicro.com/index.html";
+}
+// Function for pulling name and company when available
+// function getAffiliateName () {
+  var n = getCookieValue ("BMTMicro.Affiliates.Name");
+  var c = getCookieValue ("BMTMicro.Affiliates.Company");
+//   return (((c == null) || (c == "")) ? n : (n + ", " + c));
+// }
+//
